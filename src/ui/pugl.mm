7671059978a2c6efbdd7d27130eb84047eb71145
Michael Fisher <mfisher31@gmail.com>
2026-01-09 16:04:19 -0500

    cmake: wip: Get something building. Import Pugl directly to src dir.

---
+++ src/ui/pugl.mm
@@ -0,0 +1,7 @@
+// Pugl Objective-C amalgamation - compile pugl Objective-C files
+
+#include "pugl/src/mac.m"
+// #include "pugl/src/mac_cairo.m"    // Requires cairo
+#include "pugl/src/mac_gl.m"
+#include "pugl/src/mac_stub.m"
+// #include "pugl/src/mac_vulkan.m"   // Requires vulkan
