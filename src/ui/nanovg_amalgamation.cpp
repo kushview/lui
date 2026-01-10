354fb9070fef5e561fa2e196d50b3d04ac0736b5
Michael Fisher <mfisher31@gmail.com>
2026-01-10 06:07:22 -0500

    delete unused files.

--- src/ui/nanovg_amalgamation.cpp
+++
@@ -1,8 +0,0 @@
-// NanoVG C amalgamation - include nanovg C sources directly
-
-#define NVG_NO_STB
-#define GL_SILENCE_DEPRECATION
-
-// Include the nanovg C sources directly
-#include "../nanovg/nanovg.c"
-#include "../nanovg/nanovg_gl.c"
